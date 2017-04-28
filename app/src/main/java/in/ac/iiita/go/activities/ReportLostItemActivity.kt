b4562c9f1ab72b139b3d755eeddd39add9661d19
Ajay Agarwal <ha5463@gmail.com>
2017-04-28 22:34:25 +0530

    Added Lost Items Form

--- app/src/main/java/in/ac/iiita/go/activities/ReportLostItemActivity.kt
+++ app/src/main/java/in/ac/iiita/go/activities/ReportLostItemActivity.kt
@@ -1,26 +1,27 @@
 package `in`.ac.iiita.go.activities
 
 import `in`.ac.iiita.go.R
-import `in`.ac.iiita.go.models.FoundItem
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
-import kotlinx.android.synthetic.main.layout_found.*
+import android.view.View
 
-class ReportFoundItemActivity : AppCompatActivity() {
+class ReportLostItemActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_report_found_item)
+        setContentView(R.layout.activity_report_lost_item)
         val toolbar = findViewById(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
-        
+
         val fab = findViewById(R.id.fab) as FloatingActionButton
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+
+
     }
 }
